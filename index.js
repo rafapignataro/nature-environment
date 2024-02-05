09181c902e4d84a255ff50ae7c5b06d26da2bcd9
Rafael Pignataro <rafapignataro@gmail.com>
2024-02-05 18:03:25 -0300

    feat: refactored some code, chunks not working 100% properly yet (connection between then)

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import * as THREE from 'three';
 import { MapControls } from 'three/addons/controls/MapControls.js';
-import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+
 const DEFAULT_CONFIG = {
-  width: 300,
-  height: 300,
-  cellSize: 4,
+  size: 3,
+  chunkSize: 256,
+  resolution: 8,
   octaves: 8,
   persistance: 0.5,
   lacunarity: 2,
-  heightLimit: 50,
-  falloff: false,
+  heightMultiplier: 50,
+  islands: false,
   wireframe: false,
 }
+
 let CONFIG = { ...DEFAULT_CONFIG };
 
 const COLORS = {
@@ -22,174 +23,262 @@ const COLORS = {
   snow: new THREE.Color(0xfffafa),
 }
 
-function createSea(map) {
-  const geometry = new THREE.CircleGeometry(
-    map.width,
-    map.columns - 1
-  );
+// Utils
+function inverseLerp(min, max, value) {
+  // Garante que o value esteja dentro do intervalo de min a max
+  value = Math.max(min, Math.min(max, value));
 
-  const material = new THREE.MeshStandardMaterial({
-    color: COLORS.water,
-    side: THREE.DoubleSide,
-    wireframe: false
-  });
-
-  material.shadowSide = THREE.DoubleSide;
-  const mesh = new THREE.Mesh(geometry, material);
-
-  mesh.rotation.x = - Math.PI / 2;
-  mesh.position.y -= 0;
-  mesh.receiveShadow = true;
-
-  return mesh;
+  // Calcula o value transformado para o intervalo de 0 a 1
+  return (value - min) / (max - min);
 }
 
-function inverseLerp(min, max, valor) {
-  // Garante que o valor esteja dentro do intervalo de min a max
-  valor = Math.max(min, Math.min(max, valor));
-
-  // Calcula o valor transformado para o intervalo de 0 a 1
-  return (valor - min) / (max - min);
+function createEmptyMatrix(size) {
+  return Array.from({ length: size }).map(() => Array.from({ length: size }).fill(0));
 }
 
-class MapGenerator {
-  // Params
-  width;
+// Core
+class Noise {
+  seed;
 
-  height;
+  constructor() {
+    this.seed = Math.floor(Math.random() * 100000);
 
-  cellSize;
+    noise.seed(this.seed);
+  }
 
+  getNoise(x, z) {
+    const value = noise.simplex2(x, z);
+
+    return (value + 1) / 2;
+  }
+}
+
+class Terrain {
+  scene;
+
+  // Sizing
+  size;
+
+  chunkSize;
+
+  // Variation
   octaves;
 
   persistance;
 
   lacunarity;
 
-  heightLimit;
+  heightMultiplier;
 
-  falloff;
+  // Config
+  islands;
 
   wireframe;
 
-  offset;
+  chunks;
 
-  // Calculated
-  columns;
-
-  rows;
-
-  map;
-
-  mesh;
+  noise;
 
   minHeight;
 
   maxHeight;
 
-  constructor({ width, height, cellSize, octaves, persistance, lacunarity, heightLimit, falloff, wireframe, offset }) {
-    this.width = width;
-    this.height = height;
-    this.cellSize = cellSize;
+  constructor({
+    scene,
+    size,
+    chunkSize,
+    resolution,
+    octaves,
+    persistance,
+    lacunarity,
+    heightMultiplier,
+    islands,
+    wireframe
+  }) {
+    this.scene = scene;
+    this.size = size;
+    this.chunkSize = chunkSize;
+    this.resolution = resolution;
     this.octaves = octaves;
     this.persistance = persistance;
     this.lacunarity = lacunarity;
-    this.heightLimit = heightLimit;
-    this.falloff = falloff;
+    this.heightMultiplier = heightMultiplier;
+    this.islands = islands;
     this.wireframe = wireframe;
-
-    this.columns = Math.floor(this.width / this.cellSize);
-    this.rows = Math.floor(this.height / this.cellSize);
 
     this.minHeight = Number.MAX_SAFE_INTEGER;
     this.maxHeight = Number.MIN_SAFE_INTEGER;
 
-    this.offset = offset;
+    this.chunks = [];
 
-    this.map = this.#createMap();
+    this.noise = new Noise();
+  }
+
+  #create() {
+    for (let z = 0; z < this.size; z++) {
+      for (let x = 0; x < this.size; x++) {
+        const chunk = new Chunk({
+          size: this.chunkSize,
+          offset: { z: z * this.chunkSize, x: x * this.chunkSize },
+          resolution: this.resolution,
+          heightMultiplier: this.heightMultiplier,
+          getHeight: this.#getHeight.bind(this),
+          islands: this.islands,
+          wireframe: this.wireframe,
+        });
+
+        this.#addChunk(chunk);
+      }
+    }
+  }
+
+  #addChunk(chunk) {
+    this.chunks.push(chunk);
+    this.scene.add(chunk.mesh);
+  }
+
+  #getHeight(x, z) {
+    let amplitude = 1;
+    let frequency = 1;
+    let noiseHeight = 0;
+
+    const terrainWidth = this.size * this.chunkSize;
+
+    const normalX = x / terrainWidth;
+    const normalZ = z / terrainWidth;
+
+    const cellSize = this.chunkSize / this.resolution;
+
+    for (let i = 0; i < this.octaves; i++) {
+      const xPos = (normalX * cellSize) * frequency;  // Use x diretamente
+      const zPos = (normalZ * cellSize) * frequency;  // Use z diretamente
+
+      const value = this.noise.getNoise(xPos, zPos);
+
+      noiseHeight += value * amplitude;
+
+      amplitude *= this.persistance;
+      frequency *= this.lacunarity;
+    }
+
+    if (noiseHeight < this.minHeight) this.minHeight = noiseHeight;
+    if (noiseHeight > this.maxHeight) this.maxHeight = noiseHeight;
+
+    noiseHeight = inverseLerp(this.minHeight, this.maxHeight, noiseHeight);
+
+    return noiseHeight;
+  }
+
+  destroy() {
+    this.chunks.forEach(chunk => {
+      this.scene.remove(chunk.mesh);
+    });
+
+    this.chunks.length = 0;
+  }
+
+  update({ size, chunkSize, resolution, octaves, persistance, lacunarity, heightMultiplier, islands, wireframe }) {
+    this.size = size;
+    this.chunkSize = chunkSize;
+    this.resolution = resolution;
+    this.octaves = octaves;
+    this.persistance = persistance;
+    this.lacunarity = lacunarity;
+    this.heightMultiplier = heightMultiplier;
+    this.islands = islands;
+    this.wireframe = wireframe;
+  }
+
+  create() {
+    this.#create();
+  }
+
+  hash() {
+    return JSON.stringify({
+      size: this.size,
+      chunkSize: this.chunkSize,
+      resolution: this.resolution,
+      octaves: this.octaves,
+      persistance: this.persistance,
+      lacunarity: this.lacunarity,
+      heightMultiplier: this.heightMultiplier,
+      islands: this.islands,
+      wireframe: this.wireframe
+    })
+  }
+}
+
+class Chunk {
+  size;
+
+  offset;
+
+  resolution;
+
+  heightMultiplier;
+
+  islands;
+
+  wireframe;
+
+  getHeight;
+
+  // Calculated
+  grid;
+
+  mesh;
+
+  constructor({ size, offset, resolution, heightMultiplier, islands, wireframe, getHeight }) {
+    this.size = size;
+    this.offset = offset;
+    this.resolution = resolution;
+    this.heightMultiplier = heightMultiplier;
+    this.islands = islands;
+    this.wireframe = wireframe;
+    this.getHeight = getHeight;
+
+    this.grid = this.#createGrid();
     this.mesh = this.#createMesh();
   }
 
-  #createMap() {
-    const grid = [];
+  #createGrid() {
+    const gridSize = Math.floor(this.size / this.resolution);
 
-    let falloffGrid = this.falloff ? this.#createFalloffMap() : null;
+    const grid = createEmptyMatrix(gridSize);
 
-    for (let y = 0; y < this.rows; y++) {
-      grid[y] = [];
+    // Generate heights
+    for (let z = 0; z < gridSize; z++) {
+      for (let x = 0; x < gridSize; x++) {
+        const height = this.getHeight(
+          x + this.offset.x,
+          z + this.offset.z
+        );
 
-      for (let x = 0; x < this.columns; x++) {
-        let amplitude = 1;
-        let frequency = 1;
-        let noiseHeight = 0;
-
-        for (let i = 0; i < this.octaves; i++) {
-          const xPos = ((x * this.cellSize / this.width) - this.offset.x + (this.width / 2)) * frequency;
-          const yPos = ((y * this.cellSize / this.height) - this.offset.y + (this.height / 2)) * frequency;
-
-          const value = (perlin.get(xPos, yPos) + 1) / 2;
-
-          noiseHeight += value * amplitude;
-
-          amplitude *= this.persistance;
-
-          frequency *= this.lacunarity;
-        }
-
-        if (noiseHeight < this.minHeight) this.minHeight = noiseHeight;
-        if (noiseHeight > this.maxHeight) this.maxHeight = noiseHeight;
-
-        grid[y][x] = (noiseHeight)
-
-        if (this.falloff) grid[y][x] = grid[y][x] - falloffGrid[y][x]
+        grid[z][x] = height;
       }
     }
 
-    for (let y = 0; y < this.rows; y++) {
-      for (let x = 0; x < this.columns; x++) {
-        grid[y][x] = inverseLerp(this.minHeight, this.maxHeight, grid[y][x]);
-      }
-    }
-    console.timeEnd('frame')
+    // Normaize heights
+    // for (let y = 0; y < gridSize; y++) {
+    //   for (let x = 0; x < gridSize; x++) {
+    //     grid[y][x] = inverseLerp(this.minHeight, this.maxHeight, grid[y][x]);
+    //   }
+    // }
 
     return grid;
   }
 
-  #createFalloffMap() {
-    const size = this.rows;
-    const grid = [];
-
-    function evaluate(value) {
-      const a = 3;
-      const b = 2.2;
-
-      return Math.pow(value, a) / (Math.pow(value, a) + Math.pow(b - b * value, a))
-    }
-
-    for (let i = 0; i < size; i++) {
-      grid[i] = [];
-      for (let j = 0; j < size; j++) {
-        const x = i / size * 2 - 1;
-        const y = j / size * 2 - 1;
-
-        const value = Math.max(Math.abs(x), Math.abs(y));
-
-        grid[i][j] = evaluate(value);
-      }
-    }
-
-    return grid
-  }
-
   #createMesh() {
+    const gridSize = Math.floor(this.size / this.resolution);
+
     const geometry = new THREE.PlaneGeometry(
-      this.width,
-      this.height,
-      this.columns - 1,
-      this.rows - 1
+      this.size,
+      this.size,
+      gridSize - 1,
+      gridSize - 1
     );
 
-    geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(this.rows * this.columns * 3), 3));
+    geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(gridSize * gridSize * 3), 3));
 
     const material = new THREE.MeshStandardMaterial({
       vertexColors: true,
@@ -200,186 +289,136 @@ class MapGenerator {
       flatShading: true,
     });
 
-    material.needsUpdate = true;
-
     const positions = geometry.attributes.position;
     const colors = geometry.attributes.color;
 
-    for (let y = 0; y < this.rows; y++) {
-      for (let x = 0; x < this.columns; x++) {
-        const index = y * this.columns + x;
-        const height = this.map[y][x];
+    for (let z = 0; z < gridSize; z++) {
+      for (let x = 0; x < gridSize; x++) {
+        const index = z * gridSize + x;
+
+        const height = this.grid[z][x];
 
         const color = (() => {
-          if (height < .45) {
-            positions.setZ(index, 0.45 * this.heightLimit);
-
-            return COLORS.water;
-          }
-          positions.setZ(index, height * this.heightLimit);
-
+          if (height < .35) return COLORS.water;
           if (height < .5) return COLORS.sand;
           if (height < .7) return COLORS.grass;
           if (height < .9) return COLORS.rock;
           return COLORS.snow;
         })();
 
+        positions.setZ(index, (height < .35 ? .35 : height) * this.heightMultiplier);
         colors.setXYZ(index, color.r, color.g, color.b);
       }
     }
 
-    BufferGeometryUtils.mergeVertices(geometry)
-    geometry.computeVertexNormals();
+    const mesh = new THREE.Mesh(geometry.toNonIndexed(), material);
 
-    const mesh = new THREE.Mesh(geometry, material);
-
-    mesh.position.x = this.offset.x + (this.width / 2);
-    mesh.position.z = this.offset.y + (this.height / 2);
+    mesh.position.x = this.offset.x;
+    mesh.position.z = this.offset.z;
     mesh.rotation.x = - Math.PI / 2;
     mesh.castShadow = true;
     mesh.receiveShadow = true;
 
     return mesh;
   }
+}
 
-  hash() {
-    return JSON.stringify({
-      width: this.width,
-      height: this.height,
-      cellSize: this.cellSize,
-      octaves: this.octaves,
-      persistance: this.persistance,
-      lacunarity: this.lacunarity,
-      heightLimit: this.heightLimit,
-      falloff: this.falloff,
-      wireframe: this.wireframe
-    });
+class UIControl {
+  config;
+
+  constructor(config) {
+    this.config = config;
+
+    const gui = new dat.GUI();
+
+    const generationFolder = gui.addFolder('Generation');
+    generationFolder.open();
+
+    generationFolder.add(this.config, 'size', 1, 5, 1);
+    generationFolder.add(this.config, 'resolution', 2, 32, 2);
+    generationFolder.add(this.config, 'chunkSize', 64, 2048, 4);
+
+    generationFolder.add(this.config, 'octaves', 1, 10, 1);
+    generationFolder.add(this.config, 'persistance', 0.1, 5, 0.1);
+    generationFolder.add(this.config, 'lacunarity', 1, 10, 1);
+    generationFolder.add(this.config, 'heightMultiplier', 1, 100, 0.1);
+
+    generationFolder.add(this.config, 'islands');
+    generationFolder.add(this.config, 'wireframe');
+
+    generationFolder.add({
+      'Generate new seed': () => {
+        window.terrain.destroy();
+        window.terrain = new Terrain({ ...this.config, scene });
+
+        window.terrain.create();
+      }
+    }, 'Generate new seed');
+
   }
 }
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xeeeeee);
+// App
+function main() {
+  // Scene
+  const scene = new THREE.Scene();
+  window.scene = scene;
+  scene.background = new THREE.Color(0xeeeeee);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
-
-const renderer = new THREE.WebGLRenderer();
-
-renderer.shadowMap.enabled = true;
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-// Luz direcional
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(500, 250, -500);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
-const helper = new THREE.DirectionalLightHelper(directionalLight, 5);
-scene.add(helper);
-
-const controls = new MapControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-// Configuração da câmera
-camera.position.z -= 300;
-camera.position.y = 175;
-
-camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  // Renderer
+  const renderer = new THREE.WebGLRenderer();
+  renderer.shadowMap.enabled = true;
   renderer.setSize(window.innerWidth, window.innerHeight);
-});
+  document.body.appendChild(renderer.domElement);
 
-function createChunks(size, config) {
-  const chunks = [];
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+  camera.position.z -= 300;
+  camera.position.y = 175;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  for (let i = 0; i < size; i++) {
-    chunks[i] = [];
+  // Light
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(500, 250, -500);
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
+  const helper = new THREE.DirectionalLightHelper(directionalLight, 5);
+  scene.add(helper);
 
-    for (let j = 0; j < size; j++) {
-      const chunkOffset = { x: j * config.width, y: i * config.height };
-      console.log(chunkOffset)
-      const chunk = new MapGenerator({ ...config, offset: chunkOffset });
+  // Fly Controls
+  const controls = new MapControls(camera, renderer.domElement);
+  controls.enableDamping = true;
 
-      chunks[i][j] = chunk;
+  // UI Controller
+  new UIControl(CONFIG);
+
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+
+  window.terrain = new Terrain({ ...CONFIG, scene });
+
+  window.terrain.create();
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+
+    controls.update();
+
+    renderer.render(scene, camera);
+
+    if (window.terrain.hash() !== JSON.stringify(CONFIG)) {
+      window.terrain.destroy();
+
+      window.terrain.update(CONFIG);
+
+      window.terrain.create();
     }
-  }
+  };
 
-  return {
-    chunks,
-    render(scene) {
-      for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-          const chunk = chunks[i][j];
-
-          scene.add(chunk.mesh);
-          const arrowHelper = new THREE.ArrowHelper(
-            new THREE.Vector3(0, 1, 0),
-            new THREE.Vector3(chunk.offset.x, 0, chunk.offset.y),
-            100,
-            'red'
-          );
-          scene.add(arrowHelper);
-        }
-      }
-    },
-    destroy(scene) {
-      for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-          const chunk = chunks[i][j];
-
-          scene.remove(chunk.mesh)
-        }
-      }
-    }
-  }
+  animate();
 }
 
-let currentChunks;
-
-const animate = () => {
-  requestAnimationFrame(animate);
-
-  // Atualiza os controles
-  controls.update();
-
-  // Renderiza a cena
-  renderer.render(scene, camera);
-
-  if (!currentChunks || JSON.stringify(CONFIG) !== currentChunks.chunks[0][0].hash()) {
-    if (currentChunks) currentChunks.destroy(scene);
-
-    currentChunks = createChunks(3, CONFIG);
-
-    currentChunks.render(scene);
-  }
-};
-
-const gui = new dat.GUI();
-
-const generationFolder = gui.addFolder('Generation');
-generationFolder.open();
-
-generationFolder.add(CONFIG, 'width', 100, 3000, 100);
-generationFolder.add(CONFIG, 'height', 100, 3000, 100);
-generationFolder.add(CONFIG, 'cellSize', 2, 32, 2);
-generationFolder.add(CONFIG, 'octaves', 1, 10, 1);
-generationFolder.add(CONFIG, 'persistance', 0.1, 5, 0.1);
-generationFolder.add(CONFIG, 'lacunarity', 1, 10, 1);
-generationFolder.add(CONFIG, 'heightLimit', 1, 100, 0.1);
-generationFolder.add(CONFIG, 'falloff');
-generationFolder.add(CONFIG, 'wireframe');
-generationFolder.add({
-  'Generate new seed': () => {
-    perlin.seed();
-    if (currentChunks) {
-      currentChunks.destroy(scene)
-      currentChunks = undefined;
-    }
-
-  }
-}, 'Generate new seed');
-
-animate();
+main();
